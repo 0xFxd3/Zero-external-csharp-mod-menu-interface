d41df7ddb535a25a725f5701b7631806630e7251
0xFXD3 <37590363+0xFxd3@users.noreply.github.com>
2022-12-21 16:39:52 +0000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿
+namespace NavigationUICSHARP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Networking = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Home = new System.Windows.Forms.Panel();
+            this.HomeButton4 = new System.Windows.Forms.Button();
+            this.HomeButton3 = new System.Windows.Forms.Button();
+            this.HomeButton2 = new System.Windows.Forms.Button();
+            this.HomeButton1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.Home.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Networking
+            // 
+            this.Networking.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(12)))), ((int)(((byte)(249)))));
+            this.Networking.Enabled = false;
+            this.Networking.Location = new System.Drawing.Point(485, 147);
+            this.Networking.Name = "Networking";
+            this.Networking.Size = new System.Drawing.Size(400, 346);
+            this.Networking.TabIndex = 15;
+            this.Networking.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.panel1.Controls.Add(this.Home);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Enabled = false;
+            this.panel1.Location = new System.Drawing.Point(19, 109);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(417, 397);
+            this.panel1.TabIndex = 13;
+            // 
+            // Home
+            // 
+            this.Home.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(12)))), ((int)(((byte)(249)))));
+            this.Home.Controls.Add(this.HomeButton4);
+            this.Home.Controls.Add(this.HomeButton3);
+            this.Home.Controls.Add(this.HomeButton2);
+            this.Home.Controls.Add(this.HomeButton1);
+            this.Home.Enabled = false;
+            this.Home.Location = new System.Drawing.Point(6, 38);
+            this.Home.Name = "Home";
+            this.Home.Size = new System.Drawing.Size(400, 346);
+            this.Home.TabIndex = 6;
+            this.Home.Visible = false;
+            // 
+            // HomeButton4
+            // 
+            this.HomeButton4.Enabled = false;
+            this.HomeButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HomeButton4.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HomeButton4.ForeColor = System.Drawing.Color.White;
+            this.HomeButton4.Location = new System.Drawing.Point(3, 117);
+            this.HomeButton4.Name = "HomeButton4";
+            this.HomeButton4.Size = new System.Drawing.Size(387, 29);
+            this.HomeButton4.TabIndex = 6;
+            this.HomeButton4.Text = "Fourth";
+            this.HomeButton4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.HomeButton4.UseVisualStyleBackColor = true;
+            // 
+            // HomeButton3
+            // 
+            this.HomeButton3.Enabled = false;
+            this.HomeButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HomeButton3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HomeButton3.ForeColor = System.Drawing.Color.White;
+            this.HomeButton3.Location = new System.Drawing.Point(3, 82);
+            this.HomeButton3.Name = "HomeButton3";
+            this.HomeButton3.Size = new System.Drawing.Size(387, 29);
+            this.HomeButton3.TabIndex = 5;
+            this.HomeButton3.Text = "Third";
+            this.HomeButton3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.HomeButton3.UseVisualStyleBackColor = true;
+            // 
+            // HomeButton2
+            // 
+            this.HomeButton2.Enabled = false;
+            this.HomeButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HomeButton2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HomeButton2.ForeColor = System.Drawing.Color.White;
+            this.HomeButton2.Location = new System.Drawing.Point(3, 47);
+            this.HomeButton2.Name = "HomeButton2";
+            this.HomeButton2.Size = new System.Drawing.Size(387, 29);
+            this.HomeButton2.TabIndex = 4;
+            this.HomeButton2.Text = "Second";
+            this.HomeButton2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.HomeButton2.UseVisualStyleBackColor = true;
+            // 
+            // HomeButton1
+            // 
+            this.HomeButton1.Enabled = false;
+            this.HomeButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HomeButton1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HomeButton1.ForeColor = System.Drawing.Color.White;
+            this.HomeButton1.Location = new System.Drawing.Point(3, 12);
+            this.HomeButton1.Name = "HomeButton1";
+            this.HomeButton1.Size = new System.Drawing.Size(387, 29);
+            this.HomeButton1.TabIndex = 3;
+            this.HomeButton1.Text = "Home";
+            this.HomeButton1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.HomeButton1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(12)))), ((int)(((byte)(249)))));
+            this.button3.Location = new System.Drawing.Point(206, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 29);
+            this.button3.TabIndex = 5;
+            this.button3.Text = " ";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(12)))), ((int)(((byte)(249)))));
+            this.button4.Location = new System.Drawing.Point(309, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 29);
+            this.button4.TabIndex = 4;
+            this.button4.Text = " ";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(12)))), ((int)(((byte)(249)))));
+            this.button2.Location = new System.Drawing.Point(103, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 29);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Networking";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(12)))), ((int)(((byte)(249)))));
+            this.button1.Location = new System.Drawing.Point(0, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Home";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(12)))), ((int)(((byte)(249)))));
+            this.panel3.Enabled = false;
+            this.panel3.Location = new System.Drawing.Point(412, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(488, 406);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Enabled = false;
+            this.panel2.Location = new System.Drawing.Point(19, 29);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(417, 83);
+            this.panel2.TabIndex = 14;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.panel4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel4.BackgroundImage")));
+            this.panel4.Enabled = false;
+            this.panel4.Location = new System.Drawing.Point(19, 506);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(417, 27);
+            this.panel4.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Crimson;
+            this.ClientSize = new System.Drawing.Size(904, 562);
+            this.Controls.Add(this.Networking);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.panel1.ResumeLayout(false);
+            this.Home.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Networking;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel Home;
+        private System.Windows.Forms.Button HomeButton4;
+        private System.Windows.Forms.Button HomeButton3;
+        private System.Windows.Forms.Button HomeButton2;
+        private System.Windows.Forms.Button HomeButton1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Net;
+
+
+
+namespace NavigationUICSHARP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+
+
+        int tab = 0;
+        int selection = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.TransparencyKey = Color.Crimson;
+            Thread backgroundThread = new Thread(new ThreadStart(recieve_input));
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void tabchecker()
+        {
+            switch (tab)
+            {
+                case 1:
+                    Home.Visible = true;
+                    button2.BackColor = Color.DimGray;
+                    button1.BackColor = Color.FromArgb(255, 30, 30, 30);
+                    break;
+                case 2:
+                    button1.BackColor = Color.DimGray;
+                    button2.BackColor = Color.FromArgb(255, 30, 30, 30);
+                    Home.Visible = false;
+                    Networking.Visible = true;
+                    break;
+                case 3:
+                    button1.BackColor = Color.FromArgb(255, 30, 30, 30);
+                    button2.BackColor = Color.FromArgb(255, 30, 30, 30);
+                    Home.Visible = false;
+                    Networking.Visible = false;
+                    break;
+            }
+            if (tab == 1)// Home
+            {
+                switch (selection)
+                {
+                    case 1: //button 1
+                        HomeButton1.Focus();
+                        break;
+                    case 2:
+                        HomeButton2.Focus();
+                        break;
+                    case 3:
+                        HomeButton3.Focus();
+                        break;
+                    case 4:
+                        HomeButton4.Focus();
+                        break;
+
+                }
+            }
+        }
+
+        void commandcheck(string command)
+        {
+            if (command.Contains("Q"))
+            {
+                MessageBox.Show("a");
+                tabchecker();
+
+                tab = tab + 1;
+                if (tab > 3)
+                {
+                    tab = 0;
+                }
+            }
+        }
+
+        bool pipethread = true;
+        bool recieved_input = false;
+        string intputted;
+        void recieve_input()
+        {
+
+            while (pipethread)
+            {
+                var namedPipeServer = new NamedPipeServerStream("nhsupspipe", PipeDirection.InOut, 1, PipeTransmissionMode.Byte);
+                var streamReader = new StreamReader(namedPipeServer);
+                namedPipeServer.WaitForConnection();
+
+
+                //Console.WriteLine($"read from pipe client: {streamReader.ReadLine()}");
+                intputted = streamReader.ReadLine();
+                if (intputted.Length > 0)
+                {
+                    recieved_input = true;
+
+                }
+                else
+                {
+                    recieved_input = false;
+                }
+                commandcheck(intputted);
+                Console.WriteLine(intputted);
+                namedPipeServer.Dispose();
+            }
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Q)
+            {
+                MessageBox.Show("a");
+                tabchecker();
+
+                tab = tab + 1;
+                if (tab > 3)
+                {
+                    tab = 0;
+                }
+            }
+
+            if (e.KeyCode == Keys.W)
+            {
+                selection = selection + 1;
+                if (selection > 9)
+                {
+                    selection = 0;
+                }
+            }
+
+
+
+        }
+    }
+}
